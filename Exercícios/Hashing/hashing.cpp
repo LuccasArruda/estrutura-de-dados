d7213b6c4db06c5e2cab7566533c95fc533c7d22
luccas-arruda <luccasarruda@hotmail.com.br>
2024-03-31 13:01:59 -0300

    feat: adicionar conteúdo da aula

--- Exercícios/Hashing/hashing.cpp
+++ Exercícios/Hashing/hashing.cpp
@@ -1,32 +1,57 @@
 #include <stdio.h>
+#include <locale.h>
 
 #define QUANTIDADE_ESTADOS 27
-#define TAMANHO_HASH 54
+#define TAMANHO_HASH 97
 
 typedef struct{
 	char estado[100];
-	bool ocupado;
+	int ocupado;
 } dadosEstado;
 
-int retornaAsciiString(char )
+int retornaHash(char nomeEstado[100]){
+	int valorNomeEstado = 0, i = 0;
+	for(i; i < 100; i++){
+		valorNomeEstado += (int)(nomeEstado[i]);	
+	}
+	return (valorNomeEstado % 97);
+}
 
 int main(){
-	int i;
-	dadosEstado estados[QUANTIDADE_ESTADOS], hashTable[TAMANHO_HASH];
+	int i, colisoes = 0, indiceTabelaHash;
+	dadosEstado hashTable[TAMANHO_HASH];
 	char estado[100];
 	
-	for(i = 0; i < TAMANHO_HASH; i++)
-		hashTable[i].ocupado = false;  
+	setlocale(LC_ALL, "");
+	
+	for(i = 0; i < TAMANHO_HASH; i++){
+		hashTable[i].ocupado = 0;  
+	}
+	
 	
 	for(i = 0; i < QUANTIDADE_ESTADOS; i++){
 		gets(estado);
-		retornaAsciiString(estado);
-		gets(estados[i].estado);
+		printf("Estado: %s", estado);
+		indiceTabelaHash = retornaHash(estado);
+		printf("\n%d - ", indiceTabelaHash);
+		
+		if(hashTable[indiceTabelaHash].ocupado == 0){
+			hashTable[indiceTabelaHash].estado[100] = estado[100];
+			hashTable[indiceTabelaHash].ocupado = 1;	
+		} else {
+			colisoes++;	
+		}
 	}
 	
-	for(i = 0; i < QUANTIDADE_ESTADOS; i++){
-		printf("\n%s", estados[i].estado);
+	for(i = 0; i < TAMANHO_HASH; i++){
+		if(hashTable[i].ocupado == 1){
+			printf("\n[%d] - ocupado", i);
+		} else {
+			printf("\n[%d] - livre", i);
+		}
 	}
+	
+	printf("\ncolisões: %d", colisoes);
 	
 	return 0;
 }
